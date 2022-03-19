5e87ef366799fedfab8e503494834524d097cfda
Karol Dzialowski <karlososhd@gmail.com>
2022-03-19 16:37:53 +0100

    🚧 Fantasy main button

---
+++ src/stories/atoms/CatanButton/Button.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentMeta } from '@storybook/react';
+
+import { Button } from './Button';
+
+export default {
+  title: 'atoms/CatanButton',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+export const Primary = () => {
+    return (
+        <>
+        <Button>
+            Roll
+        </Button>
+        <Button>
+            Other button long 
+        </Button>
+        </>
+    )
+}
--- src/stories/atoms/index.ts
+++ src/stories/atoms/index.ts
@@ -1,1 +1,2 @@
 export * from './Button'
+export {Button as CatanButton } from './CatanButton'
