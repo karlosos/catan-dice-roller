7c0506c85abcbacc52c1e7d881869eeca4d0d529
Karol Dzialowski <karlososhd@gmail.com>
2022-03-22 22:22:35 +0100

    ✨ Add dice face numbers

--- src/stories/atoms/Dice/Dice.stories.tsx
+++ src/stories/atoms/Dice/Dice.stories.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { ComponentMeta } from "@storybook/react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { Dice } from "./Dice";
 
 export default {
   title: "atoms/Dice",
   component: Dice,
+  argTypes: {
+    value: { control: "number", min: 1, max: 6 },
+  },
 } as ComponentMeta<typeof Dice>;
 
-export const Primary = () => {
-  return (
-    <>
-      <Dice />
-    </>
-  );
-};
+const Template: ComponentStory<typeof Dice> = (args) => <Dice {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = { value: 3 };
--- src/stories/atoms/Dice/Dice.tsx
+++ src/stories/atoms/Dice/Dice.tsx
@@ -4,26 +4,72 @@ import styled from "styled-components";
 const DiceStyled = styled.div`
   width: 150px;
   height: 150px;
-  background-color: #eee;
+  background-color: #D5D3D8;
   border-radius: 10px;
-  border: 2px solid #292829;
+  border: 2px solid #272527;
   box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
-   transform: rotate(12deg);
 `;
 
 const Inner = styled.div`
-    background-color: white;
-    border-radius: 40px;
-    width: 100%;
-    height: 100%;
+  background-color: #FFFDFE;
+  border-radius: 40px;
+  width: 100%;
+  height: 100%;
+
+  display: grid;
+  grid-template-areas:
+    "a . c"
+    "e g f"
+    "d . b";
+
+  flex: 0 0 auto;
 `;
 
-export const Dice: React.FC = () => {
+const Pip = styled.div`
+  display: block;
+  align-self: center;
+  justify-self: center;
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  background-color: #272527;
+  box-shadow: inset 0 3px #111, inset 0 -3px #555;
+
+  &:nth-child(2) {
+    grid-area: b;
+  }
+
+  &:nth-child(3) {
+    grid-area: c;
+  }
+
+  &:nth-child(4) {
+    grid-area: d;
+  }
+
+  &:nth-child(5) {
+    grid-area: e;
+  }
+
+  &:nth-child(6) {
+    grid-area: f;
+  }
+
+  &:nth-child(odd):last-child {
+    grid-area: g;
+  }
+`;
+
+export const Dice: React.FC<{ value: number }> = ({ value }) => {
+  let pips = Number.isInteger(value)
+    ? Array(value)
+        .fill(0)
+        .map((_, i) => <Pip key={i} />)
+    : null;
+
   return (
     <DiceStyled>
-      <Inner>
-          1
-      </Inner>
+      <Inner>{pips}</Inner>
     </DiceStyled>
   );
 };
