42508ff2c0da83634fc9b509abb357ba25f2df98
Karol Dzialowski <karlososhd@gmail.com>
2022-04-12 19:38:35 +0200

    ♻️ Remove unused component. Change DICE_FACES to have all numbers.

--- src/features/mainView/MainView.tsx
+++
@@ -1,7 +0,0 @@
-export const MainView: React.FC = () => {
-    return (
-        <div>
-            This is the main app
-        </div>
-    )
-}
--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,2 +1,1 @@
-// export const DICE_FACES = [1, 2, 3, 4, 5, 6];
-export const DICE_FACES = [2, 5];
+export const DICE_FACES = [1, 2, 3, 4, 5, 6];
