cfad9c66d24b10985b59f2ba9ae721d442abf562
Karol Działowski <karlososhd@gmail.com>
2022-05-15 18:55:48 +0200

    ✅ Add useKeyPress test

---
+++ src/hooks/__tests__/useKeyPress.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { useKeyPress } from "hooks/useKeyPress";
+import React from "react";
+
+describe("useKeyPress hook", () => {
+  it("WHEN given key pressed THEN handler is called", () => {
+    // GIVEN
+    const { onKeyPress } = renderTestComponent({keyCodes: ["Enter"]})
+
+    // WHEN
+    userEvent.type(screen.getByText('TestComponent'), '{enter}')
+
+    // THEN
+    expect(onKeyPress).toBeCalled();
+  });
+
+  it("WHEN other key pressed THEN handler is not called", () => {
+    // GIVEN
+    const { onKeyPress } = renderTestComponent({keyCodes: ["Enter"]})
+
+    // WHEN
+    userEvent.type(screen.getByText('TestComponent'), '{space}')
+
+    // THEN
+    expect(onKeyPress).not.toBeCalled();
+  });
+
+  const TestComponent = ({ keyCodes, onKeyPress }: {keyCodes: string[], onKeyPress: () => void}) => {
+    useKeyPress(onKeyPress, keyCodes);
+
+    return <div>TestComponent</div>;
+  };
+
+  const renderTestComponent = ({keyCodes}: {keyCodes: string[]}) => {
+    const onKeyPress = jest.fn();
+
+    render(<TestComponent onKeyPress={onKeyPress} keyCodes={keyCodes} />);
+
+    return {
+      onKeyPress,
+    };
+  };
+});
