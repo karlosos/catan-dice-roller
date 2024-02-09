521683acba757658e54ac97bac4d10b0f241af1d
Karol Dzialowski <karlososhd@gmail.com>
2024-02-10 00:57:08 +0100

    🚧 WIP: token with value

--- src/stories/molecules/DiceContainer/DiceContainer.style.tsx
+++ src/stories/molecules/DiceContainer/DiceContainer.style.tsx
@@ -93,3 +93,110 @@ export const Die2Styled = styled.div<{ animationName: Keyframes }>`
     }
   }}
 `;
+
+export const slideTopNumericalValue = keyframes`
+  0% {
+    transform: translateX(-50%) translateY(-300px) rotate(0deg);
+  }
+  30% {
+    transform: translateX(-50%) translateY(5px) rotate(0deg);
+  }
+  100% {
+    transform: translateX(-50%) translateY(0px) rotate(0deg);
+  opacity: 1;
+  }
+`;
+
+export const scaleDownNumericalValue = keyframes`
+  0% {
+    transform: translateX(-50%) translateY(0px) rotate(0deg);
+  }
+  100% {
+    transform: translateX(-50%) scale(0.5) rotate(0deg);
+    opacity: 0;
+  }
+`;
+
+export const TokenContainer = styled.div<{ animationName: Keyframes }>`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  top: 30px;
+  opacity: 0;
+
+  animation-name: ${(props) => props.animationName};
+  ${(props) => {
+    if (props.animationName === scaleDownNumericalValue) {
+      return css`
+        animation-duration: 0.2s;
+        animation-iteration-count: 1;
+        animation-timing-function: linear;
+        animation-fill-mode: both;
+      `;
+    } else {
+      return css`
+        animation-delay: 1.5s;
+        animation-duration: 0.5s;
+        animation-iteration-count: 1;
+        animation-timing-function: ease-out;
+        animation-fill-mode: both;
+      `;
+    }
+  }}
+`;
+
+// TODO move to atoms
+// TODO: black copy from pips from dice
+export const Token = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-shadow: none;
+  width: 64px;
+  height: 64px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f7d69c;
+  border-radius: 100%;
+  box-shadow: 0 2px 0 rgb(149 50 9 / 58%), 0 3px 4px rgb(129 41 8 / 20%),
+    0 0 6px rgb(92 26 4 / 51%);
+`;
+
+export const TokenValue = styled.div<{ isRed: boolean }>`
+  font-size: 52px;
+  line-height: 0.8em;
+  font-family: Times New Roman;
+  font-weight: bold;
+  color: #1e0a00;
+
+  ${(props) => {
+    if (props.isRed) {
+      return css`
+        color: #c00020;
+      `;
+    }
+  }}
+`;
+
+export const Pips = styled.div`
+  display: flex;
+  gap: 2px;
+`;
+
+export const Pip = styled.div<{ isRed: boolean }>`
+  display: block;
+  align-self: center;
+  justify-self: center;
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+  background-color: #1e0a00;
+
+  ${(props) => {
+    if (props.isRed) {
+      return css`
+        background-color: #c00020;
+      `;
+    }
+  }}
+`;
--- src/stories/molecules/DiceContainer/DiceContainer.tsx
+++ src/stories/molecules/DiceContainer/DiceContainer.tsx
@@ -7,6 +7,13 @@ import {
   scaleDown2,
   slideTop1,
   slideTop2,
+  TokenContainer,
+  slideTopNumericalValue,
+  scaleDownNumericalValue,
+  Token,
+  Pip,
+  Pips,
+  TokenValue,
 } from "./DiceContainer.style";
 
 export const DiceContainer: React.FC<{
@@ -14,14 +21,60 @@ export const DiceContainer: React.FC<{
   die2: number;
   showDice: boolean;
 }> = ({ die1, die2, showDice }) => {
+  const sum = die1 + die2;
+  const isRed = sum === 8 || sum === 6;
+
   return (
-    <DiceContainerStyled>
-      <Die1Styled animationName={showDice ? slideTop1 : scaleDown1}>
-        <Die value={die1} />
-      </Die1Styled>
-      <Die2Styled animationName={showDice ? slideTop2 : scaleDown2}>
-        <Die value={die2} />
-      </Die2Styled>
-    </DiceContainerStyled>
+    <>
+      <DiceContainerStyled>
+        <Die1Styled animationName={showDice ? slideTop1 : scaleDown1}>
+          <Die value={die1} />
+        </Die1Styled>
+        <Die2Styled animationName={showDice ? slideTop2 : scaleDown2}>
+          <Die value={die2} />
+        </Die2Styled>
+      </DiceContainerStyled>
+      <TokenContainer
+        animationName={
+          showDice ? slideTopNumericalValue : scaleDownNumericalValue
+        }
+      >
+        <Token>
+          <TokenValue isRed={isRed}>{sum}</TokenValue>
+          <Pips>
+            {[...Array(valueToPips(sum))].map((_, idx) => (
+              <Pip isRed={isRed} key={idx} />
+            ))}
+          </Pips>
+        </Token>
+      </TokenContainer>
+    </>
   );
+};
+
+const valueToPips = (value: number): number => {
+  switch (value) {
+    case 2:
+      return 1;
+    case 3:
+      return 2;
+    case 4:
+      return 3;
+    case 5:
+      return 4;
+    case 6:
+      return 5;
+    case 8:
+      return 5;
+    case 9:
+      return 4;
+    case 10:
+      return 3;
+    case 11:
+      return 2;
+    case 12:
+      return 1;
+    default:
+      return 0;
+  }
 };
