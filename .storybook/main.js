a560afba1420435933e54e3ff63764b9ce91e8c7
Karol Dzialowski <karlososhd@gmail.com>
2024-02-26 20:14:26 +0100

    ⬆️  Upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,15 +10,23 @@ module.exports = {
     ];
     return config;
   },
+
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-mdx-gfm"
   ],
-  framework: "@storybook/react",
-  core: {
-    builder: "webpack5",
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
+
+  docs: {
+    autodocs: true
+  }
 };
